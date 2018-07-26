317e166733ccb4603369739e6a41e24813e9be7a
Vlad Yaremenko <vladyaremenko95.vy@gmail.com>
2018-07-27 00:13:29 +0300

    Init angular cli

--- cli.js
+++ cli.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
+const path = require('path');
+const execa = require('execa');
+const Listr = require('listr');
 const inquirer = require('inquirer');
-const program = require('commander');
-
-program
-  .version('0.0.1');
+// const program = require('commander');
+//
+// program
+//   .version('0.0.1');
 
 const QUESTIONS = [{
-  name: 'project-name',
+  name: 'title',
   type: 'input',
   message: 'Project name:',
+  default: __dirname.split(path.sep).pop(),
   validate(input) {
     if (/^([A-Za-z\-\_\d])+$/.test(input)) {
       return true;
@@ -17,8 +21,44 @@ const QUESTIONS = [{
       return 'Project name may only include letters, numbers, underscores and hashes.';
     }
   }
+}, {
+  name: 'description',
+  type: 'input',
+  message: 'Project description:',
+  default: ''
+}, {
+  name: 'type',
+  type: 'list',
+  message: 'Project type:',
+  choices: [
+    {name: 'Plain JS', value: 'plain-js'},
+    {name: 'Angular', value: 'angular'},
+    {name: 'React', value: 'react'},
+    {name: 'Vue', value: 'vue'},
+    {name: 'Three.js', value: 'three'},
+    {name: 'Chrome-extension', value: 'chrome_extension'},
+    {name: 'Mapbox', value: 'Mapbox'}
+  ]
 }];
 
 inquirer.prompt(QUESTIONS).then((answers) => {
   console.log(answers);
+
+  new Listr([
+    {
+      title: `Init ${answers.title} project`,
+      task: () => {
+        switch (answers.type) {
+          case 'angular': {
+            return require('@angular/cli').default({
+              cliArgs: ['new', answers.title, '--style', 'scss']
+            });
+          }
+          default: {
+            throw new TypeError('Invalid project type!');
+          }
+        }
+      }
+    }
+  ]).run();
 });
