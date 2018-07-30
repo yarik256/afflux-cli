82d25c87299003fca04f877875ab88768813fddf
Vlad Yaremenko <vladyaremenko95.vy@gmail.com>
2018-07-30 22:51:31 +0300

    Update test env

---
+++ tests/main.test.js
@@ -0,0 +1,9 @@
+// TODO: Create tmp directory
+// TODO: Open tmp directory
+// TODO: Create project
+
+test('Project directory was created', () => {
+  expect(true).toBe(true);
+});
+
+// TODO: Remove tmp directory
