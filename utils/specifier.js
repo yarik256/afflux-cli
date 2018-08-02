69732e2db4d1e11493edcbad347fe56b1b495464
Vlad Yaremenko <vladyaremenko95.vy@gmail.com>
2018-08-02 14:57:09 +0300

    Implement specifier tests

--- utils/specifier.js
+++ utils/specifier.js
@@ -1,24 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 
-// Copy .gitignore
 module.exports.copyGitignore = function (to) {
+  if (!to) throw new Error('Target directory is required!');
+
   fs.copyFileSync(
     path.join(__dirname, '../specification/files/.gitignore'),
     path.join(to, '.gitignore')
   );
 }
 
-// Copy .editorconfig
 module.exports.copyEditorconfig = function (to) {
+  if (!to) throw new Error('Target directory is required!');
+
   fs.copyFileSync(
     path.join(__dirname, '../specification/files/.editorconfig'),
     path.join(to, '.editorconfig')
   );
 }
 
-// Copy .stylelintrc
 module.exports.copyStylelintrc = function (to) {
+  if (!to) throw new Error('Target directory is required!');
+
   fs.copyFileSync(
     path.join(__dirname, '../specification/files/.stylelintrc'),
     path.join(to, '.stylelintrc')
