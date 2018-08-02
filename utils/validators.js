fc6179121900d33f0bbe6e8817269813c515eaae
Vlad Yaremenko <vladyaremenko95.vy@gmail.com>
2018-08-02 15:04:54 +0300

    Implement validator tests

--- utils/validators.js
+++ utils/validators.js
@@ -1,5 +1,5 @@
 module.exports.title = (input) => {
-  if (/^([A-Za-z\-\_\d])+$/.test(input)) {
+  if (input && /^([A-Za-z\-\_\d])+$/.test(input)) {
     return true;
   } else {
     return 'Project name may only include letters, numbers, underscores and hashes.';
